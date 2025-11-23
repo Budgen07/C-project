8d3989098e04a81ee078821ca080c4d3e5fee36d
jaibadani <93388618+jaibadani@users.noreply.github.com>
2025-11-23 22:37:28 +0530

    Delete storage.h

--- storage.h
+++
@@ -1,8 +0,0 @@
-#ifndef STORAGE_H   // <--- MAKE SURE THIS SAYS STORAGE_H
-#define STORAGE_H   // <--- MAKE SURE THIS SAYS STORAGE_H
-
-void loadData();
-void saveData();
-void exportItinerary();
-
-#endif
